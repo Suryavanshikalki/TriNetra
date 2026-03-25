b5535db588954e5bef982a6c2c4eca932a8f11cb
Suryavanshikalki <suryabhai19966@gmail.com>
2026-03-26 03:25:42 +0530

    Create fileUploader.js

---
+++ backend/utils/fileUploader.js
@@ -0,0 +1,16 @@
+// File: backend/utils/fileUploader.js
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Universal download folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
