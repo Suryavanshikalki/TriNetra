ff4fb3e4181b49cc2142450e818221e37fa5a3ab
Suryavanshikalki <suryabhai19966@gmail.com>
2026-03-29 01:50:14 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
-
-// 👁️ TRINETRA FIX: Yahan chhota 'c' kar diya gaya hai
 import { UserProvider } from './context/Usercontext.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
