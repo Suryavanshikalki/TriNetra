34bb1672b0fc7b247f914f367c8c01bbfddbcbaa
Suryavanshikalki <suryabhai19966@gmail.com>
2026-03-28 21:16:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // 👁️ TriNetra Fix: Vite ko batana ki frontend folder me dhundhe
+  resolve: {
+    alias: {
+      '@': '/frontend/src',
+    },
+  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
   }
 });
