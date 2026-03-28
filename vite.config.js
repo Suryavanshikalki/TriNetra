62ecd3514f6c0d483bf663f33e357f024fc8b5a7
Suryavanshikalki <suryabhai19966@gmail.com>
2026-03-29 01:44:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // 👁️ TRINETRA FIX: Ye setting double 'src' ke chakkar ko bypass karegi
-      '@': path.resolve(__dirname, './src'),
-    },
+    // 👁️ TRINETRA FIX: Ye setting small/capital aur path dono ko sambhaal legi
     extensions: ['.js', '.jsx', '.json']
-  },
-  server: {
-    port: 3000,
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true
   }
 });
