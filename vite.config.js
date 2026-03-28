a479eb315de6beea69d5d8a76e07f439c8c378a5
Suryavanshikalki <suryabhai19966@gmail.com>
2026-03-28 21:09:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: { 
+  build: {
     outDir: 'dist',
-    emptyOutDir: true 
+    emptyOutDir: true
   }
 });
